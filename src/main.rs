4179f6a9af8dcf06e2b8d54db8f603ec9c0979aa
alts_alt_ <unrelatedalt@gmail.com>
2025-03-16 18:08:01 -0700

    feat: add env var settings

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,19 @@ mod router;
 mod static_server;
 mod utils;
 
-use crate::router::Router;
-
 use http::{Response, StatusCode};
 use hyper::server::conn::http1;
 use hyper_util::rt::TokioIo;
+use router::Router;
 use static_server::StaticServer;
 use types::{RouterFuture, RouterRequest};
 use utils::create_response_body_from_string;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
+
+const PORT_ENV_VAR: &str = "PORT";
+const ROOT_ENV_VAR: &str = "STATIC_SERVER_ROOT";
+const FALLBACK_FILE_ENV_VAR: &str = "STATIC_SERVER_FALLBACK_FILE";
 
 fn ping_endpoint(_: RouterRequest) -> RouterFuture {
 	let body = create_response_body_from_string("pong".to_string());
@@ -25,17 +28,36 @@ fn ping_endpoint(_: RouterRequest) -> RouterFuture {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let address = SocketAddr::from(([127, 0, 0, 1], 8080));
-    let listener = TcpListener::bind(address).await?;
+    // initialize environment
+    let port: u16 = match std::env::var(PORT_ENV_VAR) {
+        Ok(env_var) => env_var.parse::<u16>().unwrap_or(8080),
+        Err(_) => 8080,
+    };
 
+    let static_server_root: String = match std::env::var(ROOT_ENV_VAR) {
+        Ok(env_var) => env_var,
+        Err(_) => "build".to_string(),
+    };
+
+    let static_server_fallback_file: String = match std::env::var(FALLBACK_FILE_ENV_VAR) {
+        Ok(env_var) => env_var,
+        Err(_) => "build/404.html".to_string(),
+    };
+
+    // initialize service components
     let mut static_server = StaticServer::new();
-    static_server.with_root("build".to_string());
-    static_server.with_fallback_file("build/test2.txt".to_string());
+    static_server.with_root(static_server_root);
+    static_server.with_fallback_file(static_server_fallback_file);
 
 	let mut router = Router::new();
-	router.with_route_fn(vec!("ping".to_string()), ping_endpoint);
+	router.with_route_fn(vec!("api".to_string(), "ping".to_string()), ping_endpoint);
     router.with_service(vec!(), static_server);
 
+    let address = SocketAddr::from(([127, 0, 0, 1], port));
+    let listener = TcpListener::bind(address).await?;
+
+
+    // listen for requests
     loop {
         let (stream, _) = listener.accept().await?;
         let io = TokioIo::new(stream);
