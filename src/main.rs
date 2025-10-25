70638953aaa53a9a99c4213943f9b85fb388d54c
a1ts-a1t <unrelatedalt@gmail.com>
2025-10-25 09:05:22 -0700

    feat: image endpoint

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,9 @@ use rocket::{catch, catchers, get, launch, routes};
 use std::path::Path;
 use twitch::twitch_handler;
 
-use crate::kennel::{creature_handler, init_kennel, kennel_handler};
+use crate::kennel::{
+    creature_handler, creature_img_handler, init_kennel, kennel_handler, kennel_img_handler,
+};
 
 #[catch(404)]
 async fn not_found() -> Option<NamedFile> {
@@ -29,14 +31,15 @@ fn rocket() -> _ {
     let (kennel, kennel_cleanup) = init_kennel();
     rocket::build()
         .mount(
-            "/api",
+            "/api/kennel-club",
             routes![
-                ping_handler,
-                twitch_handler,
                 kennel_handler,
-                creature_handler
+                kennel_img_handler,
+                creature_handler,
+                creature_img_handler,
             ],
         )
+        .mount("/api", routes![ping_handler, twitch_handler,])
         .mount("/", FileServer::from("./static"))
         .register("/", catchers![not_found])
         .manage(Cache::<String, String>::default())
