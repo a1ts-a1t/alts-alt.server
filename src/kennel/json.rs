a624be8a523d2581996dc304c3124ed1aaf073a2
a1ts-a1t <unrelatedalt@gmail.com>
2025-11-01 15:32:27 -0700

    feat: redirect endpoint

--- src/kennel/json.rs
+++ src/kennel/json.rs
@@ -4,6 +4,8 @@ use serde::Serialize;
 #[derive(Serialize)]
 pub struct CreatureJson {
     id: String,
+    url: String,
+    display_name: String,
     radius: f64,
     position: Vec2,
     sprite_path: String,
@@ -14,9 +16,17 @@ impl From<&Creature> for CreatureJson {
         let sprite_path = format!("/api/kennel-club/{}/img", creature.id);
         CreatureJson {
             id: creature.id.clone(),
+            url: creature.url.clone(),
+            display_name: creature.display_name.clone(),
             radius: creature.radius,
             position: creature.position,
             sprite_path,
         }
+    }
+}
+
+impl CreatureJson {
+    pub fn url(&self) -> String {
+        self.url.clone()
     }
 }
